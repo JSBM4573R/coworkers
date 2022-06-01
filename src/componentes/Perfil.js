a9724fcdb3a66008f8121de086bfd454930ca6f1
Juan Sebastian <juan_seb07@hotmail.com>
2022-05-31 23:54:28 -0500

    Se mejora el responsive desing y se agregan otros estilos en los css

--- src/componentes/Perfil.js
+++ src/componentes/Perfil.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../css/Perfil.css';
 
 // Componente funcional
-//Se incluyen propiedades a mi componente funcional con props
+// Se incluyen propiedades a mi componente funcional con props
 function Perfil(props) {
   // Un componente retorna una estructura en JSX
   return (
@@ -15,7 +15,6 @@ function Perfil(props) {
         <p className="nombre-perfil"><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className="cargo-perfil">{props.cargo} en <strong>{props.empresa}</strong></p>
         <p className="texto-perfil">"{props.descripcion}"</p>
-        {/* <p className="texto-perfil">Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p> */}
       </div>
     </div>
   );
